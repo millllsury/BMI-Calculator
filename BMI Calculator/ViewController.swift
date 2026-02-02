78bccd2fe7dd16b0e93ba38aa642ba0a7a0cdc72
millllsury <katyabastrikina@mail.ru>
2026-02-02 16:21:48 +0500

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Ekaterina Bastrikina on 02/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMI CalculatorTests/BMI_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorTests.swift
+//  BMI CalculatorTests
+//
+//  Created by Ekaterina Bastrikina on 02/02/2026.
+//
+
+import Testing
+@testable import BMI_Calculator
+
+struct BMI_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
